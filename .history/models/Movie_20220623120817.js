60b476559ac5b3d1045e10fbb550b4b246399ea3
Josephmar Diaz <josephmar@diaz.com>
2022-06-23 12:24:24 -0400

    cree y consumi una base de datos por primera vez en mi vida

---
+++ .history/models/Movie_20220623120817.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const MovieSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        require:[true, "Por favor ingrese el titulo"]
+    },
+    plot:{
+        type:String,
+        require:[true, "Por favor ingrese el titulo"]
+    }
+})
---
+++ models/Movie.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const MovieSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        require:[true, "Por favor ingrese el titulo"]
+    },
+    plot:{
+        type:String,
+        require:[true, "Por favor ingrese la descripcion"]
+    }
+})
+
+export default mongoose.models.Movie || mongoose.model('Movie',MovieSchema)
